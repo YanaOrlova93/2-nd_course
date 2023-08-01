309564b2ae4e04e863216c46143af72c4f36db1c
YanaOrlova93 <orlova93zm@mail.ru>
2023-08-01 12:36:57 +1200

    fix js

--- hw2/script.js
+++ hw2/script.js
@@ -8,27 +8,42 @@ let js = 'Brendan Eich'; //3
 alert(js);
 
 
-let c = Number(10); //4
-let b = Number(2);
-let result = c-b;
-alert(result);
+//=============================
+let c = 10; //4
+let b = 2;
+let resultSum = c+b;
+alert(resultSum);
+let c1 = 10; //4
+let b1 = 2;
+let resultMin = c1-b1;
+alert(resultMin);
+let c2 = 10; //4
+let b2 = 2;
+let resultUm = c2*b2;
+alert(resultUm);
+let c3 = 10; //4
+let b3 = 2;
+let resultDel = c3/b3;
+alert(resultDel);
+//===========================
 
-let d = Number(2**5); //5
+let d = (2**5); //5
 let result1 = d;
 alert(result1);
 
-let a1 = 9; //6
-let b1 = 2;
-let result2 = a1%b1;
+let a4 = 9; //6
+let b4 = 2;
+let result2 = a4%b4;
 alert(result2);
+
 
 let num = 1; //7
 num += 5;
 num -= 3;
 num *= 7;
 num /= 3;
-num += 1;
-num -= 1;
+num++;
+num--;
 alert(num);
 
 
@@ -39,9 +54,8 @@ const user = { //9
     name: 'Yana',
     age1: 29,
     isAdmin: true,
-    cityOfResidence: 'Moscow',
 }
-user.cityOfResidence;
+user.cityOfResidence = 'Moscow';
 user.age1 = 30;
 delete user.cityOfResidence;
 let info = prompt('Какую информацию хотие узнать о пользователе?');
