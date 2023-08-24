7121ca3500ca926fa62c4db93f2cb0103c2efa5b
YanaOrlova93 <orlova93zm@mail.ru>
2023-08-24 10:58:38 +0300

    firstcommhw7

---
+++ hw7/script.js
@@ -0,0 +1,89 @@
+//Задание 1
+
+const answerRight = prompt ('Ответ');
+const answer = 'строка';
+if (answerRight.toUpperCase() === answer.toUpperCase()) {
+    alert('Это правильный ответ');
+} else {
+    alert('Не угадал');
+}
+
+
+//Задание 2
+function searchStart(['Кошка', 'Кит', 'Кот', 'Собака'], 'ко');
+let search = 'ко';
+searchStart.forEach((search) => {
+	
+	if (product.toLowerCase().includes(search.toLowerCase())) {
+		console.log(search);
+	}
+});
+
+
+//Задание 3
+
+console.log(Math.floor(32.58884)); //32
+console.log(Math.ceil(32.58884)) //33
+console.log(Math.round(32.58884)); //33
+
+
+//Задание 4
+
+//52, 53, 49, 77, 21, 32.
+
+alert( Math.max(52, 53, 49, 77, 21, 32) ); // 77
+alert( Math.min(52, 53, 49, 77, 21, 32)); // 21
+
+//Задание 5 
+
+function getRandomInt(minValue, maxValue) {
+    return Math.round(Math.random() * maxValue);
+}
+console.log(getRandomInt(0, 10));
+
+
+
+//Задание 6
+function getRandomArrNumbers(); // [ 
+Math.random 
+// от 0 до 12, длина массива 12 / 2 = 6
+
+
+
+//Задание 7
+
+function newArray(x, y) {
+  let array = [];
+  for (let i = x; i <= y; i++) array.push(i);
+ 
+  return  Math.random(array);
+}       
+
+//Задание 8
+let currentDate = new Date();
+console.log(currentDate);
+
+
+
+//Задание 9
+
+let currentDate1 = new Date(2023, 8, 24);
+currentDate1.setDate(currentDate1() + 72);
+console.log(currentDate1);
+
+
+//Задание 10
+
+
+
+
+//Задание 11
+
+  
+
+
+//Задание 12
+
+
+   //Задание13
+   
